1d79964c8880a3b4f333591214755ce1648f93ba
Shanna <sg0989463@otc.edu>
2026-04-12 15:03:44 -0500

    cleared up some issues where creating a report was not attaching the species to the report and updated format of sporeprint entries

--- understory/fieldnotes/views.py
+++ understory/fieldnotes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import ForagingReport
+from .models import ForagingReport, ForagingReportSpecies
 from .forms import ForagingReportForm
 from basket.basket import Basket
 
@@ -8,14 +8,24 @@ from basket.basket import Basket
 @login_required
 def report_create(request):
     """Create a new foraging report for the logged-in user."""
+    basket = Basket(request)
+
     if request.method == 'POST':
         form = ForagingReportForm(request.POST)
         if form.is_valid():
             report = form.save(commit=False)
             report.user = request.user
             report.save()
-            Basket(request).clear()
+            # Save each species in the basket as a ForagingReportSpecies item
+            for item in basket:
+                ForagingReportSpecies.objects.create(
+                    report=report,
+                    species=item['species'],
+                    quantity=1,
+                )
+            basket.clear()
             return redirect('fieldnotes:report_detail', pk=report.pk)
+            
     else:
         form = ForagingReportForm()
     return render(request, 'fieldnotes/report_create.html', {'form': form})
